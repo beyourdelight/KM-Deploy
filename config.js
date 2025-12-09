cc18aaca638edf7c25087f2ccf9a7d37946d60c7
beyourdelight <chutikarn.ruangp@gmail.com>
2025-12-09 07:51:01 +0700

    config api

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 // };
 
 const CONFIG = {
-    API_URL: "https://relief-boxed-crest-arbitrary.trycloudflare.com",  
-    MEDIA_URL: "https://relief-boxed-crest-arbitrary.trycloudflare.com" 
+    API_URL: "https://url-technologies-safely-bags.trycloudflare.com ",  
+    MEDIA_URL: "https://url-technologies-safely-bags.trycloudflare.com " 
 };
